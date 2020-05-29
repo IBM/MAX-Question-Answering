948e88f674e12f3074cc9f898a5db697f344fe7e
Brendan Dwyer <brendan.dwyer@ibm.com>
2020-05-29 11:28:08 -0700

    re-add Hong's suggestion

--- api/predict.py
+++ api/predict.py
@@ -75,7 +75,7 @@ class ModelPredictAPI(PredictAPI):
         input_json = MAX_API.payload
         try:
             for p in input_json["paragraphs"]:
-                if not frozenset(p.keys()) == frozenset(["context", "questions"]):
+                if frozenset(p.keys()) != frozenset(["context", "questions"]):
                     abort(400, f"Invalid input paragraph keys {list(p.keys())}, please provide a context and questions.")
                 if p["context"] == "":
                     abort(400, "Invalid input, please provide a paragraph.")
